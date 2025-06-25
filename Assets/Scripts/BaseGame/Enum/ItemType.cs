4b7a6ce730e01418b78d27d6ab95a4301eb12894
kyosuke <kn1359028527@gmail.com>
2025-06-25 15:05:11 +0900

    アイテム用のEnumを作成

---
+++ Assets/Scripts/BaseGame/Enum/ItemType.cs
@@ -0,0 +1,30 @@
+public enum ItemType
+{
+    // ベッドルームのドアを開けるアイテム
+    BedRoomDoorKey,
+    // 廊下のドアを開けるアイテム
+    CorridorDoorKey,
+    // ドライバー
+    Driver,
+    // リモコン
+    RemoController,
+    // 銀の鳥の像
+    SilverBirdStatue,
+    // 金の鳥の像
+    GoldBirdStatue,
+    // ワインボトル
+    WineBottle,
+    // 白いマグカップ
+    WhiteMugCup,
+    // ベッドルームの棚の鍵
+    BedLoomDrawerKey,
+    // 日付メモ
+    DateMemo,
+    // ボール
+    Ball,
+    // 記号メモ
+    MarkMemo,
+    // 外にでる鍵
+    LastRoomOutKey
+
+}
